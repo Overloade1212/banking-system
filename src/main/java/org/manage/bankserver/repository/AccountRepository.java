11fd4793e70ecd260186cd9ead3c08fa83372c45
Overloadedq <artleonid20022509@gmail.com>
2026-04-17 08:32:36 +0300

    feat: add AccountRepository with username lookup

---
+++ src/main/java/org/manage/bankserver/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package org.manage.bankserver.repository;
+
+import org.manage.bankserver.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AccountRepository extends JpaRepository<Account, UUID>{
+    Optional<Account> findByUsername(String username);
+}
