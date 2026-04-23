0ab23439d71ff97b12b36a9c194d030b72e3bea8
Overloadedq <artleonid20022509@gmail.com>
2026-04-23 16:49:36 +0300

    chore:changes in Account Service

--- src/main/java/org/manage/bankserver/dto/AccountResponse.java
+++ src/main/java/org/manage/bankserver/dto/AccountResponse.java
@@ -1,18 +1,19 @@
 package org.manage.bankserver.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class AccountResponse {
-    private Long id;
-    private String accountNumber;
+
+    private UUID id;
     private String username;
+    private BigDecimal balance;
     private String type;
-    private Double balance;
+    private LocalDateTime createdAt;
 }
--- src/main/java/org/manage/bankserver/service/AccountService.java
+++ src/main/java/org/manage/bankserver/service/AccountService.java
@@ -2,11 +2,14 @@ package org.manage.bankserver.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.manage.bankserver.dto.AccountResponse;
 import org.manage.bankserver.dto.RegisterAccountRequest;
 import org.manage.bankserver.entity.Account;
 import org.manage.bankserver.entity.enums.AccountType;
 import org.manage.bankserver.repository.AccountRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @RequiredArgsConstructor
 @Service
@@ -14,18 +17,26 @@ import org.springframework.stereotype.Service;
 public class AccountService  {
     private  final AccountRepository repository;
 
-     public void registerAccount(RegisterAccountRequest request){
+    public AccountResponse registerAccount(RegisterAccountRequest request){
 
-         if (repository.existsByUsername(request.getUsername())) {
-             //TODO: Заменить на свой AccauntAlreadyExistsExeption()
-             throw new RuntimeException("Such account already exists!");
-         }
+        if (repository.existsByUsername(request.getUsername())) {
+            throw new RuntimeException("Such account already exists!");
+        }
 
-         Account entity = Account.builder()
-                 .username(request.getUsername())
-                 .type(AccountType.valueOf(request.getType()))
-                 .build();
+        Account entity = Account.builder()
+                .username(request.getUsername())
+                .type(AccountType.valueOf(request.getType()))
+                .balance(BigDecimal.ZERO) // обязательно!
+                .build();
 
-         repository.save(entity);
-     }
+        Account saved = repository.save(entity);
+
+        return AccountResponse.builder()
+                .id(saved.getId())
+                .username(saved.getUsername())
+                .balance(saved.getBalance())
+                .type(saved.getType().name())
+                .createdAt(saved.getCreatedAt())
+                .build();
+    }
 }
